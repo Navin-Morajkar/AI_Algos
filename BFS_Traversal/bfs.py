d23cffc794ae6b4109f1ffdf05bf596a321ca2c1
Navin-Morajkar <navinsika@gmail.com>
2023-03-20 00:56:41 +0530

    bfs dfs updated

---
+++ BFS_Traversal/bfs.py
@@ -0,0 +1,51 @@
+# Enter number of nodes in the graph
+num_nodes = int(input("Enter the total number of nodes: "))
+
+# Create an empty dictionary to represent the graph
+graph = {}
+
+# Loop through each node & ask for its children
+for i in range(num_nodes):
+    node = input(f"\nEnter the name of node {i+1}: ")
+    children = input(f"Enter the children of node {node}: ").split()
+    graph[node] = children
+
+print("\nThe graph is: ")
+print(graph)
+
+
+# BFS function
+def bfs(graph, start, goal):
+    visited = []
+    queue = [start]
+    while queue:
+        node = queue.pop(0)
+        if node in goal:
+            print("Goal Found: ", node)
+            visited.append(node)
+            print("Traversal Order: ", visited)
+            return "Success"
+        if node not in visited:
+            visited.append(node)
+            neighbors = graph[node]
+            for neighbor in neighbors:
+                queue.append(neighbor)
+    print("Goal Not Found")
+    print("Traversal Order:", visited)
+    return visited
+
+
+# Function calls with diff i/p
+zero_goal = []
+print("\nZero Goal Nodes case: ")
+bfs(graph, 'A', zero_goal)
+
+print("\nSingle Goal Node case: ")
+single_goal = input("Enter node for single goal node case: ")
+print("Your single goal node list has the node: ", single_goal)
+bfs(graph, 'A', single_goal)
+
+print("\nMultiple Goal Nodes case: ")
+multi_goal = input("Enter nodes for multiple goal node case: ").split()
+print("The multiple goal nodes are ", multi_goal)
+bfs(graph, 'A', multi_goal)
